d14663b4847f85da49140e2119a8005f36d6b8ee
cparmar <chirag-parmar@live.com>
2020-04-05 19:06:38 -0400

    changes

---
+++ src/test/java/com/trainstation/configuration/TrainStationApplicationTests.java
@@ -0,0 +1,34 @@
+package com.trainstation.configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+
+import com.trainstation.controllers.LandingController;
+import com.trainstation.services.TrainStationService;
+
+@SpringBootTest
+class TrainStationApplicationTests {
+
+	@Autowired
+	private LandingController ladingController;
+	
+	@Autowired
+	private ErrorController errorController;
+
+	@Autowired
+	private TrainStationService trainStationService;
+	
+	@DisplayName("Context load test for landingcontroller, trainstationservice and errorcontroller")
+	@Test
+	public void contexLoads() throws Exception {
+		assertThat(ladingController).isNotNull();
+		assertThat(errorController).isNotNull();
+		assertThat(trainStationService).isNotNull();
+	}
+
+}
